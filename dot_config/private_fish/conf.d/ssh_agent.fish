9f182b59cae6baf9f9f5ec98a4c3cfc48bf59153
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2025-05-15 22:25:45 -0700

    be smarter about when we add ssh keys

--- dot_config/private_fish/conf.d/ssh_agent.fish
+++ dot_config/private_fish/conf.d/ssh_agent.fish
@@ -1,6 +1,9 @@
 ensure_ssh_agent_started
 
-# Add keys if there are none added already.
-if not ssh-add -l > /dev/null
-  ssh-add
+if test -n "$SSH_AGENT_PID"
+  # Add keys if there are none added already.
+  if not ssh-add -l > /dev/null
+    echo "ssh-add..."
+    ssh-add
+  end
 end
